2d101be1f6ff6bb1250aef31a23f30b8daeae3e6
tangi343 <ignatsokolow200914@gmail.com>
2026-09-24 20:40:37 +0500

    Refactor: Lesson model to use TimeOfDay for startTime and endTime

--- lib/features/schedule/lesson.dart
+++ lib/features/schedule/lesson.dart
@@ -1,17 +1,28 @@
+import 'package:flutter/material.dart';
+
 class Lesson {
-  final String title, time;
+  final String title;
   final int dayOfWeek;
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
 
   const Lesson({
     required this.title,
-    required this.time,
+    required this.startTime,
+    required this.endTime,
     required this.dayOfWeek,
   });
 
-  Lesson copyWith({String? title, String? time, int? dayOfWeek}) {
+  Lesson copyWith({
+    String? title, 
+    TimeOfDay? startTime, 
+    TimeOfDay? endTime, 
+    int? dayOfWeek
+  }) {
     return Lesson(
       title: title ?? this.title,
-      time: time ?? this.time,
+      startTime: startTime ?? this.startTime,
+      endTime: endTime ?? this.endTime,
       dayOfWeek: dayOfWeek ?? this.dayOfWeek,
     );
   }
